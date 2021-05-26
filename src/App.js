ff6274669541ac03cc400e8e43335b12a5c41010
Gitthuma <george.githuma@gmail.com>
2021-05-26 20:41:07 +0300

    Deleted app component and pasted it in App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/*Paste App component from index.js*/
+
+/*Create class with App compenent*/
+
+/*Add mandatory render method inside App component*/
+
+/*Add return fucntion inside render method*/
+
+/*Add return function inside render method*/
+
+/*Add div with className App inside return function*/
+
+/*Add h1 tag with hello world! inside div*/
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -6,27 +6,9 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-/*Create class with App compenent*/
+/*Delete app component and paste it in App.js*/
 
-/*Add mandatory render method inside App component*/
 
-/*Add return fucntion inside render method*/
-
-/*Add return function inside render method*/
-
-/*Add div with className App inside return function*/
-
-/*Add h1 tag with hello world! inside div*/
-
-class App extends Component {
-  render() {
-    return(
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    )
-  }
-}
 
 /*Render the App to the HTML div with class root using ReactDOM*/
 
