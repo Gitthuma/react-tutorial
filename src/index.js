8310ef5cb0815f9029fd24189a813574401decb7
Gitthuma <george.githuma@gmail.com>
2021-05-26 20:45:08 +0300

    Remove Component from React import statement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 
 /*Import react Component and remove React.Component from the extends in line 21*/
 
-import React, {Component} from 'react'
+/*Remove Component from React import statement*/
+
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
