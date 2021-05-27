48e6f5296c9c8aa7bac2d46d4158459c04abef3b
Gitthuma <george.githuma@gmail.com>
2021-05-27 23:17:54 +0300

    Add render return function with component content to be rendered

--- src/Table.js
+++ src/Table.js
@@ -6,10 +6,46 @@ import React, {Component} from 'react'
 
 /*Add manadatory render method to render our component to HTML*/
 
+/*Add render return function with component content to be rendered*/
+
 class Table extends Component {
 
     render() {
-        
+
+        return (
+
+            <table>
+
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    <tr>
+                        <td>Charlie</td>
+                        <td>Janitor</td>
+                    </tr>
+
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+
+                    <tr>
+                        <td>Dennis</td>
+                        <td>Bartender</td>
+                    </tr>
+
+                </tbody>
+
+            </table>
+
+        )
+
     }
 
 }
